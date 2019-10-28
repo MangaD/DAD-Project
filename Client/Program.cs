f752b42ad3758529bf583d454699b15a2deee107
MangaD <davidmanuel1993@hotmail.com>
2019-10-28 14:11:20 +0000

    Parser class

--- Client/Program.cs
+++ Client/Program.cs
@@ -12,6 +12,7 @@ namespace Client
 {
     class Program
     {
+        private static IServer server;
 
         public static void Main(string[] args)
         {
@@ -24,63 +25,38 @@ namespace Client
 
             string filename = args[0];
 
-            Parser parser = new Parser(filename);
-            parser.Parse();
-
+            // Get client port and name
             Console.WriteLine("Insert PORT: ");
             int cliPort = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Insert Name: ");
             string cliName = Console.ReadLine();
 
-            Client client = new Client(cliPort, cliName);
-            TcpChannel cliChannel = client.ClientListening();
-            client.clientSaysHello();
+            // Connect to server
+            server = (IServer)Activator.GetObject(typeof(IServer), "tcp://localhost:65000/MSServer");
+            //List<string> messages = server.RegisterClient(port.ToString());
+            server.RegisterClient(cliPort, cliName);
+
+            Parser parser = new Parser(filename, server);
+            parser.Parse();
+
+
+            TcpChannel channel = new TcpChannel(cliPort);
+            ChannelServices.RegisterChannel(channel, false);
+            ClientServices services = new ClientServices();
+            RemotingServices.Marshal(services, "MSClient",
+                typeof(ClientServices));
+
+            server.clientSaysHelloToServer(cliPort);
 
 
             //RunCommands();
 
             Console.ReadKey();
         }
-               
+
 
     }
 
-    public class Client
-    {
-        //public const int CLIENT_PORT = 55001;
-        //public const string CLIENT_NAME = "LEO";
-        private int clientPort;
-        private string clientName;
-        private TcpChannel cliChannel;
-        private static IServer server;
-
-        public Client(int cp, string cn)
-        {
-            this.clientPort = cp;
-            this.clientName = cn;
-        }
-
-        public TcpChannel ClientListening() {
-            TcpChannel channel = new TcpChannel(clientPort);
-            ChannelServices.RegisterChannel(channel, false);
-            ClientServices services = new ClientServices();
-            RemotingServices.Marshal(services, "MSClient",
-                typeof(ClientServices));
-
-            server = (IServer)Activator.GetObject(typeof(IServer), "tcp://localhost:65000/MSServer");
-            //List<string> messages = server.RegisterClient(port.ToString());
-            server.RegisterClient(clientPort, clientName);
-            this.cliChannel = channel;
-
-            return channel;
-        }
-
-        public void clientSaysHello()
-        {
-            server.clientSaysHelloToServer(clientPort);
-        }
-
-    }
 
     public class ClientServices : MarshalByRefObject, IClient
     {
