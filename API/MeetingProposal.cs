1cfcce1c17e90828aaf9b756cb88ddbff0779b19
André Moreira <fixenet@hotmail.com>
2019-11-06 14:14:56 +0000

    moved MeetingProposal to API

---
+++ API/MeetingProposal.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API
+{
+    [Serializable]
+    public class MeetingProposal
+    {
+        public MeetingProposal(string coordinatorURL, string topic, uint minAttendees, List<Slot> slots, List<string> invitees)
+        {
+            CoodinatorURL = CoodinatorURL;
+            Topic = topic;
+            MinAttendees = minAttendees;
+            Slots = slots;
+            Invitees = invitees;
+            ClientsJoined = new Dictionary<string, string>();
+            State = 0;
+        }
+
+        public string CoodinatorURL { get; set; }
+        public string Topic { get; set; }
+        public uint MinAttendees { get; set; }
+        public List<Slot> Slots { get; set; }
+        public List<string> Invitees { get; set; }
+        public Dictionary<string, string> ClientsJoined { get; set; }
+        public int State { get; set; } //0 - open, 1- closed, ...
+
+        public void AddSlotToSlots(Slot s) { Slots.Add(s); }
+        public void AddInviteeToInvitees(string i) { Invitees.Add(i); }
+        public void JoinClientToMeeting(string clientName, string clientURL, int slotCount, List<Slot> slots) { 
+            ClientsJoined.Add(clientName, clientURL);
+            for(int i=0; i<slotCount; i++)
+            {
+                Slots.Add(slots[i]);
+            }
+        }
+    }
+}
