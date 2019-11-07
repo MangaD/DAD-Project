1d1780312eef0c2d7d8208f1d832bdef27d8ccfc
André Moreira <fixenet@hotmail.com>
2019-11-07 15:02:42 +0000

    updated MeetingProposal

--- API/MeetingProposal.cs
+++ API/MeetingProposal.cs
@@ -15,6 +15,7 @@ namespace API
             Slots = slots;
             Invitees = invitees;
             ClientsJoined = new Dictionary<string, string>();
+            ClientSlots = new Dictionary<string, List<Slot>>();
             State = 0;
         }
 
@@ -25,12 +26,14 @@ namespace API
         public List<Slot> Slots { get; set; }
         public List<string> Invitees { get; set; }
         public Dictionary<string, string> ClientsJoined { get; set; }
+        public Dictionary<string, List<Slot>> ClientSlots { get; set; }
         public int State { get; set; } //0 - open, 1- closed, ...
 
         public void AddSlotToSlots(Slot s) { Slots.Add(s); }
         public void AddInviteeToInvitees(string i) { Invitees.Add(i); }
         public void JoinClientToMeeting(string clientName, string clientURL, int slotCount, List<Slot> slots) { 
             ClientsJoined.Add(clientName, clientURL);
+            ClientSlots.Add(clientName, slots);
             for(int i=0; i<slotCount; i++)
             {
                 Slots.Add(slots[i]);
